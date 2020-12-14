7eee39f9f16911f29815b622231233000ba752c0
kimmstop <kimmstop@gmail.com>
2020-12-14 11:21:15 +0000

    소수 찾기

--- Basic_mathematics/BOJ_1978.cpp
+++ Basic_mathematics/BOJ_1978.cpp
@@ -4,46 +4,46 @@
 
 using namespace std;
 
-int g_numofinput;
-int g_num_of_prime_in_input;
+int numofinput;
+int num_of_prime_in_input;
 
-void Isprime(double p_numtocheck_isprime);
-bool Isremain_zero(double p_numtocheck_isprime, int devider);
-bool Is_p_numtocheck_is_one(double p_numtocheck_isprime);
+void Isprime(double numtocheck_isprime);
+bool Isremain_zero(double numtocheck_isprime, int devider);
+bool Is_numtocheck_is_one(double numtocheck_isprime);
 void Increase_num_of_prime_in_input();
 
 int main()
 {
-	cin >> g_numofinput;
+	cin >> numofinput;
 	double numtocheck_isprime;
-	for(int i =0; i < g_numofinput; i++){
+	for(int i =0; i < numofinput; i++){
 		cin >> numtocheck_isprime;
-		if(Is_p_numtocheck_is_one(numtocheck_isprime) == true)
+		if(Is_numtocheck_is_one(numtocheck_isprime) == true)
 			continue;
 		Isprime(numtocheck_isprime);
 	}
 	
-	cout << g_num_of_prime_in_input;
+	cout << num_of_prime_in_input;
 }
 
-void Isprime(double p_numtocheck_isprime)
+void Isprime(double numtocheck_isprime)
 {
-	int root_of_numtocheck = static_cast<int>(sqrt(p_numtocheck_isprime));
+	int root_of_numtocheck = static_cast<int>(sqrt(numtocheck_isprime));
 	bool flag_to_check_num_isprime = true;
 	for(int devider = 2; devider <= root_of_numtocheck; devider++){
-		if(Isremain_zero(p_numtocheck_isprime, devider) == true){
+		if(Isremain_zero(numtocheck_isprime, devider) == true){
 			flag_to_check_num_isprime = false;
 		}
 	}
 	if(flag_to_check_num_isprime == true){
-		cout << p_numtocheck_isprime;
+		cout << numtocheck_isprime;
 		Increase_num_of_prime_in_input();
 	}
 }
 
-bool Isremain_zero(double p_numtocheck_isprime, int devider)
+bool Isremain_zero(double numtocheck_isprime, int devider)
 {
-	if(static_cast<int>(p_numtocheck_isprime) % devider == 0){
+	if(static_cast<int>(numtocheck_isprime) % devider == 0){
 		return true;
 	}
 	return false;
@@ -51,12 +51,12 @@ bool Isremain_zero(double p_numtocheck_isprime, int devider)
 
 void Increase_num_of_prime_in_input()
 {
-	g_num_of_prime_in_input++;
+	num_of_prime_in_input++;
 }
 
-bool Is_p_numtocheck_is_one(double p_numtocheck_isprime)
+bool Is_numtocheck_is_one(double numtocheck_isprime)
 {
-	if(static_cast<int>(p_numtocheck_isprime == 1))
+	if(static_cast<int>(numtocheck_isprime == 1))
 		return true;
 	return false;
 }
@@ -73,3 +73,9 @@ bool Is_p_numtocheck_is_one(double p_numtocheck_isprime)
 -> 아직 함수와 변수, 지역변수와 전역변수를 구분하는 나만의 방법을 
 정립하지 못해서 코드를 짜는 동안 변수 이름으로 헷갈렸다. 
 전역과 지역을 구분할때, 전역은 앞에 "g_"를 붙였다.*/
+
+/*2020-12-14
+ * 변수 앞에 변수의자료형니나 특서을 붙이는 헝가리식 표기법은 좋지 않다
+ * -> 전역변수, 인자로 넘겨주는 변수에 헝가리식 표기법을 제거 했다.
+ *  sed -i 's/변경해야하는내용/변경할 내용/g' file 
+ *  */
