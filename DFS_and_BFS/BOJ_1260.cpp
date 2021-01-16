f7afa41395563e262a9022e1c85c19befc4ba6fb
kimmstop <kimmstop@gmail.com>
2021-01-16 08:10:24 +0000

    DFS와 BFS

--- DFS_and_BFS/BOJ_1260.cpp
+++ DFS_and_BFS/BOJ_1260.cpp
@@ -1,88 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
 
-int map[1001][1001] = {0};
-int N, M, startv;
-int top=0, stack[1000] = {0}, visit[1001]= {0}; 
-int head=0,tail=0, queue[1001] = {0}; 
+using namespace std;
 
-void push(int v1)
-{
-	stack[top++] = v1;
-	if(top == 1000)
-        	printf("stack over\n");
-}
+stack <int> v_stack;
+queue <int> v_queue;
+list <int> adj_list[1001];
 
-void pop()
-{
-	top--;
-	if(top == -1)
-		printf("stack error");
-	exit(0);
-}
 
-void DFS(int v)
-{
-	push(v);
-	printf("%d ", v);
-	visit[v] = 1;
-	for(int i = 1 ; i <= N; i++){
-		if(map[v][i] == 1 && visit[i] == 0){ 
-			DFS(i);
-		}
-	}
-}
 
-void insert(int v)
-{
-	queue[tail++] = v;	
-	if(tail == 1001)
-		tail = 0;
-}
+int num_of_v, num_of_e, start_v;
+int graph[1001][1001];
+int visit[1001];
 
-int delete()
-{
-	int ret = queue[head];
-	head++;
-	if(head == 1001)
-		head = 0;
-	return ret;
-}
-
-void BFS(int v)
-{
-	
-	printf("%d ", v);
-	visit[v] = 1;
-	for(int i = 1; i <= N; i++){
-		if(map[v][i] == 1 && visit[i] == 0){
-			insert(i);
-			visit[i] = 1;
-		}
-	}
-	if(head == tail){
-		exit(0);
-	}
-	int nextv = delete();
-	
-	BFS(nextv);
-}
-
+void Do_DFS(int start_v);
+void Do_BFS(int start_v);
+void Reset_visited_vertex();
+bool Is_there_path(int v1, int v2);
+bool Is_v_not_visited(int v);
 
 int main()
 {
+	cin >> num_of_v >> num_of_e >> start_v;
 	int v1, v2;
-	
-	scanf("%d %d %d", &N, &M, &startv);
-	for(int i = 0; i < M; i++){
-		scanf(" %d %d",&v1, &v2);
-		map[v1][v2] = 1;
-		map[v2][v1] = 1;
-	
+	for(int i = 1; i <= num_of_e; i++){
+		cin >> v1 >> v2;
+		adj_list[v1].push_back(v2);
+		adj_list[v2].push_back(v1);
+		graph[v1][v2] = 1;
+		graph[v2][v1] = 1;
 	}
-	DFS(startv);
-	memset(visit,0,4004);
-	printf("\n");
-	BFS(startv);
+	v_stack.push(start_v);
+	visit[start_v] = 1;
+	Do_DFS(start_v);
+	
+	cout << "\n";
+	
+	Reset_visited_vertex();
+	
+	
+	v_queue.push(start_v);
+	cout << start_v << " ";
+	visit[start_v] = 1;
+	Do_BFS(start_v);
 }
+
+void Reset_visited_vertex()
+{
+	fill(visit, visit + 1001, 0);
+}
+
+
+bool Is_there_path(int v1, int v2)
+{
+	if(graph[v1][v2] == 1)
+		return true;
+	return false;
+}
+
+bool Is_v_not_visited(int v)
+{
+	if(visit[v] == 0)
+		return true;
+	return false;
+}
+
+
+void Do_DFS(int start_v)
+{
+	cout << start_v << " ";
+
+	int next_v;
+	for(int next_v = 1; next_v <= num_of_v; next_v++){
+		if(Is_there_path(start_v, next_v) && Is_v_not_visited(next_v)){
+			visit[next_v] = 1;
+			v_stack.push(next_v);
+			Do_DFS(next_v);
+		}
+	}	
+	
+	if(v_stack.empty())
+		return;
+	
+	v_stack.pop();
+}
+
+
+void Do_BFS(int start_v)
+{
+	int next_v;
+	for(int next_v = 1; next_v <= num_of_v; next_v++){
+		if(Is_there_path(start_v, next_v) && Is_v_not_visited(next_v)){
+			visit[next_v] = 1;
+			cout << next_v << " ";
+			v_queue.push(next_v);
+		}
+	}
+	
+	if(v_queue.empty())
+		return;
+	
+	v_queue.pop();
+		
+	next_v = v_queue.front();
+	Do_BFS(next_v);
+}
+
+
+/*2020-01-16
+Refactoring
+DFS와 BFS에서 종료되는 기저 조건 코드를 어디에 위치 시켜야 할 지
+고민했다. 상관없다. 맨 위에만 놓지 않으면 된다. */
