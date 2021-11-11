ddef70d7a05a3b24db79e6bf9bb5f4c0cd4445e8
kimmstop <kimmstop@gmail.com>
2021-11-12 02:11:59 +0900

    포도주 시식

---
+++ BOJ_prob/DP/python/BOJ_2156.py
@@ -0,0 +1,18 @@
+import sys
+total_num = int(sys.stdin.readline())
+dp_list = [0 for i in range(total_num)]
+select_list = [0 for i in range(total_num)]
+amount = []
+for i in range(total_num):
+    amount.append(int(sys.stdin.readline()))
+
+amount.insert(0,0)
+amount.insert(0,0)
+amount.insert(0,0)
+dp_list.insert(0,0)
+dp_list.insert(0,0)
+dp_list.insert(0,0)
+for i in range(3, total_num + 3):
+    t = max(dp_list[i -3] + amount[i -1] + amount[i], dp_list[i-2] + amount[i])
+    dp_list[i] = max(dp_list[i -1], t)
+print(dp_list[total_num + 2])
