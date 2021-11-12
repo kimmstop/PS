0bee98c9aa9727eb4a0135cfe1d8063073676720
kimmstop <kimmstop@gmail.com>
2021-11-12 20:28:12 +0900

    가장 긴 증가하는 부분 수열

---
+++ BOJ_prob/DP/python/BOJ_11053.py
@@ -0,0 +1,12 @@
+#*
+import sys
+length = int(sys.stdin.readline())
+num_list = list(map(int, sys.stdin.readline().split()))
+dp_list = [0 for i in range(length)]
+
+for i in range(length):
+    for j in range(i + 1):
+        if num_list[j] < num_list[i] and dp_list[j] > dp_list[i]:
+            dp_list[i] = dp_list[j]
+    dp_list[i] += 1
+print(max(dp_list))
