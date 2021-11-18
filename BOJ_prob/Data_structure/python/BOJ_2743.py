0bfb6d0fd22ca879e2832dbab60a559aa0668b53
kimmstop <kimmstop@gmail.com>
2021-11-18 20:19:13 +0900

    ROT13

--- BOJ_prob/Data_structure/python/BOJ_2743.py
+++ BOJ_prob/Data_structure/python/BOJ_2743.py
@@ -1,3 +1,15 @@
 import sys
-s = input()
-print(len(s))
+s = list(sys.stdin.readline().rstrip())
+for i in range(len(s)):
+    if s[i].isupper():
+        t = ord(s[i]) + 13
+        if t > 90:
+            t -= 26
+        s[i] = chr(t)
+    if s[i].islower():
+        t = ord(s[i]) + 13
+        if t > 122:
+            t -= 26
+        s[i] = chr(t)
+for i in range(len(s)):
+    print(s[i], end= '')
