6ad176bc78fc97ef96ad2e8ceda62608515a5584
kimmstop <kimmstop@gmail.com>
2021-11-25 18:15:07 +0900

    최대공약수와 최소공배수

--- BOJ_prob/Basic_mathematics/python/BOJ_2609.py
+++ BOJ_prob/Basic_mathematics/python/BOJ_2609.py
@@ -1,12 +1,5 @@
 import sys
+import math
 num1, num2 = map(int,sys.stdin.readline().split())
-gcd, lcm = 0, 0
-for i in range(1, min(num1, num2) + 1):
-    if num1 % i == 0 and num2 % i == 0:
-        gcd = i
-for i in range(max(num1, num2), num1 * num2 + 1, max(num1, num2)):
-    if i % num1 == 0 and i % num2 == 0:
-        lcm = i
-        break
-print(gcd)
-print(lcm)
+print(math.gcd(num1, num2))
+print(math.lcm(num1, num2))
