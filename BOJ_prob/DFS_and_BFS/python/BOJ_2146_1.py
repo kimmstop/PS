ffce358a15a3b4494fcee136bb921b20d59cb745
kimmstop <46364133+kimmstop@users.noreply.github.com>
2021-12-05 17:21:23 +0900

    다리 만들기

--- BOJ_prob/DFS_and_BFS/python/BOJ_2146_1.py
+++ BOJ_prob/DFS_and_BFS/python/BOJ_2146_1.py
@@ -7,7 +7,6 @@ def isInBound(r, c):
         return True
     return False
 
-
 def BFS(r, c):
     island = []
     visit[r][c] = 1
@@ -22,30 +21,37 @@ def BFS(r, c):
                 mapinfo[nr][nc] = islandID
                 queue.append((nr, nc))
 
-def BFS2(cur_islandID):
-    global min_dist
+
+
+def appendLand():
+    min_dist = 987654321
+    dist = 0
     queue = deque([])
-    dist = [[0 for j in range(mapsize)] for i in range(mapsize)]
     for i in range(mapsize):
         for j in range(mapsize):
-            if mapinfo[i][j] == cur_islandID:
+            if mapinfo[i][j] < 0:
                 queue.append((i, j))
-                dist[i][j] = 0
     while queue:
-        cur_r, cur_c = queue.popleft()
-        for i in range(4):
-            nr = cur_r + dir_r[i]
-            nc = cur_c + dir_c[i]
-            if isInBound(nr, nc):
-                if mapinfo[nr][nc] > 0 and mapinfo[nr][nc] != cur_islandID:
-                    min_dist = min(min_dist, dist[cur_r][cur_c])
-                    return
-                if mapinfo[nr][nc] == 0 and dist[nr][nc] == 0:
-                    dist[nr][nc] = dist[cur_r][cur_c] + 1
-                    queue.append((nr, nc))
-
-
-
+        q_len = len(queue)
+        dist += 1
+        for i in range(q_len):
+            cur_r, cur_c = queue.popleft()
+            for i in range(4):
+                nr = cur_r + dir_r[i]
+                nc = cur_c + dir_c[i]
+                if isInBound(nr, nc):
+                    if mapinfo[nr][nc] == 0:
+                        mapinfo[nr][nc] = mapinfo[cur_r][cur_c]
+                        queue.append((nr, nc))
+                    elif mapinfo[nr][nc] != 0 and mapinfo[nr][nc] != mapinfo[cur_r][cur_c]:
+                        if mapinfo[nr][nc] < mapinfo[cur_r][cur_c]:
+                            if dist == 1:
+                                min_dist = min(min_dist, 2)
+                            else:
+                                min_dist = min(min_dist, (dist -1) * 2)
+                        else:
+                            min_dist = min(min_dist,dist * 2 - 1)
+    print(min_dist)
 
 mapsize = int(sys.stdin.readline())
 mapinfo = []
@@ -54,14 +60,18 @@ for i in range(mapsize):
 dir_r = [0, 1, 0, -1]
 dir_c = [1, 0, -1, 0]
 visit = [[0 for j in range(mapsize)] for i in range(mapsize)]
-islandID = 1
+islandID = -1 
 for i in range(mapsize):
     for j in range(mapsize):
         if visit[i][j] == 0 and mapinfo[i][j] == 1:
             mapinfo[i][j] = islandID
             BFS(i, j)
-            islandID += 1
-min_dist = 987654321
-for i in range(1, islandID + 1):
-    BFS2(i)
-print(min_dist)
+            islandID -= 1
+appendLand()
+#반례였던것
+#1 1 1 1 1
+#1 0 0 0 1
+#1 0 1 0 1
+#1 0 0 0 1
+#1 1 1 1 1
+
