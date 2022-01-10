07e2e86d5065be938e5a0f34d30375336861a0b6
kimmstop <46364133+kimmstop@users.noreply.github.com>
2022-01-10 21:27:34 +0900

    트리의 지름

---
+++ BOJ_prob/DFS_and_BFS/C++/BOJ_1167.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int vertex_num;
+list<list<int>> tree;
+
+
+int main()
+{
+    cin >> vertex_num;
+    list<int>::iterator iter;
+    for(iter = tree.begin(); iter != tree.end(); iter++){
+        edgeinfo = 0;
+        while(edgeinfo != -1){
+            cin >> edgeinfo;
+            iter.push_back(edgeinfo);
+        }
+    }
+}
