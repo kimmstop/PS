448a0925912c64f1c7a4b599449712f7d564c110
kimmstop <46364133+kimmstop@users.noreply.github.com>
2022-01-11 14:09:21 +0900

    트리의 지름

--- BOJ_prob/DFS_and_BFS/C++/BOJ_1167.cpp
+++ BOJ_prob/DFS_and_BFS/C++/BOJ_1167.cpp
@@ -1,21 +1,77 @@
 #include <iostream>
+#include <vector>
 #include <list>
-
+#include <stack>
 using namespace std;
 
-int vertex_num;
-list<list<int>> tree;
+struct Node
+{
+    int num;
+    int dist;
+};
 
+int vertex_num;
+int longest_dist;
+int longest_vertex;
+bool visit[100001];
+vector <Node> tree[100001];
+
+bool isvisit(int num)
+{
+    if(visit[num] == true) return true;
+    else return false;
+}
+
+void dfs(int num, int cur_dist)
+{
+    visit[num] = true;
+    for(int i = 0; i < tree[num].size(); i++){
+        if(!isvisit(tree[num][i].num)){
+            dfs(tree[num][i].num, cur_dist + tree[num][i].dist);
+        }
+    }
+    if(cur_dist > longest_dist){
+        longest_dist = cur_dist;
+        longest_vertex = num;
+    }
+}
+
+void init_visit()
+{
+    for(int i = 1; i <= vertex_num; i++){
+        visit[i] = false;
+    }
+}
 
 int main()
 {
     cin >> vertex_num;
-    list<int>::iterator iter;
-    for(iter = tree.begin(); iter != tree.end(); iter++){
-        edgeinfo = 0;
-        while(edgeinfo != -1){
-            cin >> edgeinfo;
-            iter.push_back(edgeinfo);
+    for(int i = 0; i < vertex_num; i++){
+        int v1, v2;
+        cin >> v1;
+        while(1){
+            cin >> v2;
+            if(v2 == -1) break;
+            int dist = 0;
+            cin >> dist;
+            tree[v1].push_back({v2, dist});
+            tree[v2].push_back({v1, dist});
         }
     }
+    /*
+    for(int i = 1; i < vertex_num; i++){
+        for(int j = 0; j < tree[i].size(); j++){
+            printf("%d :", i);
+            printf("| %d, %d |", tree[i][j].num, tree[i][j].dist);
+        }
+    }
+    */
+    for(int i = 1; i <= vertex_num; i++){
+        if(visit[i] == 0){
+            dfs(i, 0);
+            init_visit();
+            dfs(longest_vertex, 0);
+        }
+    }
+    printf("%d", longest_dist);
 }
